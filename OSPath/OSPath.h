bb7e03dc576c2a9b97a3e245420def45f23e5987
Sergio Vieira <sergiosvieira@gmail.com>
2013-12-06 11:02:17 -0300

    Initial Commit

---
+++ OSPath/OSPath.h
@@ -0,0 +1,13 @@
+//
+//  OSPath.h
+//  OSPath
+//
+//  Created by Sergio Vieira on 06/12/13.
+//  Copyright (c) 2013 Sergio Vieira. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OSPath : NSObject
+
+@end
---
+++ OSPath/OSPath.m
@@ -0,0 +1,13 @@
+//
+//  OSPath.m
+//  OSPath
+//
+//  Created by Sergio Vieira on 06/12/13.
+//  Copyright (c) 2013 Sergio Vieira. All rights reserved.
+//
+
+#import "OSPath.h"
+
+@implementation OSPath
+
+@end
